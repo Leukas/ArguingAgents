e23081761a08b663a7efd58080e6c12f6b7a952d
nachi9211 <nachi9211@gmail.com>
2018-09-18 05:37:16 +0530

    1

---
+++ data.py
@@ -0,0 +1,20 @@
+# data.py
+import os
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision import datasets
+
+# Configure data loader
+def MNIST_dataset(batch_size):
+	os.makedirs('../../data/mnist', exist_ok=True)
+	dataloader = torch.utils.data.DataLoader(
+	    datasets.MNIST('../../data/mnist', 
+	        train=True, 
+	        download=True,
+	        transform=transforms.Compose([
+	            transforms.ToTensor(),
+	            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+	        ])),
+	    batch_size=batch_size, shuffle=True)
+	return dataloader
---
+++ networks/discriminator.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+from . import device
+
+class Discriminator(nn.Module):
+    def __init__(self, input_shape):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(input_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+
+        return validity

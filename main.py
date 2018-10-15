da16dafa252d61d3574971f0c16e047eefb6032e
J.L. Edman <s3435059@fwn-bborg-edu-74-213.fmnsedu.rug.nl>
2018-10-15 14:23:58 +0200

    fix for multichannel imgs

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ parser.add_argument('--batch_size', type=int, default=16, help='size of the batc
 parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
 parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
 parser.add_argument('--latent_dim', type=int, default=300, help='size of latent dimension')
-parser.add_argument('--channels', type=int, default=3, help='number of image channels')
+parser.add_argument('--channels', type=int, default=1, help='number of image channels')
 parser.add_argument('--sample_interval', type=int, default=400, help='interval betwen image samples')
 args = parser.parse_args()
 print(args)
 
 # img_shape = (args.channels, args.img_size, args.img_size)
 # img_shape = (1, 430, 128)
-# dataloaders = get_MNIST_dataloaders(args.batch_size)
-dataloaders = get_CIFAR10_dataloaders(args.batch_size)
+dataloaders = get_MNIST_dataloaders(args.batch_size)
+# dataloaders = get_CIFAR10_dataloaders(args.batch_size)
 # dataloaders = get_EEG_dataloaders(args.)
 
 
@@ -38,8 +38,8 @@ classifier = Classifier(args.channels, 10).to(device)
 gan = GAN(generator, discriminator, classifier).to(device)
 
 # Load previous save
-model_path = './models/cifar10/test300.pt'
-# model_path = './models/mnist/test4.pt'
+# model_path = './models/cifar10/test300.pt'
+model_path = './models/mnist/test_nc.pt'
 # model_path = './models/mnist/test_video2.pt'
 if os.path.exists(model_path):
     gan.load_state_dict(torch.load(model_path))
@@ -47,4 +47,4 @@ if os.path.exists(model_path):
 # print(torch.cuda.is_available())
 
 train_cgan(gan, dataloaders['train'], epochs=args.n_epochs, sample_interval=2000, latent_dim=args.latent_dim, save_path=model_path)
-# visualize_gan(gan.eval(), dataloaders['test'], visualize_fake=True)
+# visualize_gan(gan.eval(), dataloaders['test'], visualize_fake=False)
